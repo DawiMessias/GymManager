f875165b97fbb4c0404686f5365f1ba9b8174745
Dabid <wti123@gmail.com>
2020-06-28 17:37:15 -0300

    routes from members and instructors

--- routes.js
+++ routes.js
@@ -8,5 +8,13 @@ routes.get("/", function(req, res) {
     return res.send("OKEI =P ")
 })
 
+routes.get("/instructors", function(req, res) {
+    return res.render("instructors/index")
+})
+
+routes.get("/members", function(req, res) {
+    return res.send("OKEI =P ")
+})
+
 
 module.exports = routes
