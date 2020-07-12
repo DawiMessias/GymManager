dca525d32d7f15b3c385140bba2616323f23a9a0
david <wtih123@gmail.com>
2020-07-11 22:11:40 -0300

    Using FS to store information and perform treatment

--- instructors.js
+++ instructors.js
@@ -1,9 +1,18 @@
+const fs = require("fs")
 exports.post = function(req, res) {
   const keys = Object.keys(req.body)
 
-  for (key of keys) {
-        if ( req.body[keys] == "")
-            return res.send("Please, fill all fields")
+  for(key of keys) {
+    if ( req.body[key] == "")
+      return res.send("Please, fill all fields")
   }
+
+  fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+    if (err) 
+      return res.send("Write File error!")
+    
+      return res.redirect("/instructors")
+    
+  })
   return res.send(req.body)
 }
