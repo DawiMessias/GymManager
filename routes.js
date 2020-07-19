a982dcf225efc5649b633f221accc56ad4440511
david <wtih123@gmail.com>
2020-07-18 21:22:06 -0300

    routes from parameter

--- routes.js
+++ routes.js
@@ -6,6 +6,8 @@ routes.get("/instructors", function(req, res) {
     return res.render("instructors/index")
 })
 
+routes.get("/instructors/:id", instructors.show)
+
 routes.get("/instructors/create", function(req, res) {
     return res.render("instructors/create")
 })
