b2e74e2eeb4d1243576b8056895042a9cf5d6114
david <wtih123@gmail.com>
2020-08-01 17:18:54 -0300

    Creating formatting and showing on the front

--- utils.js
+++ utils.js
@@ -1,15 +1,25 @@
 module.exports = {
-  age: function age(timestamp) {
+  age: function(timestamp) {
     const today = new Date()
     const birthDate = new Date(timestamp)
  
     let age = today.getFullYear() - birthDate.getFullYear()
     const month = today.getMonth - birthDate.getMonth()
  
-    if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+    if (month < 0 || month == 0 && today.getDate() <= birthDate.getDate()) {
       age = age - 1
     }
  
     return age
+  },
+  date: function(timestamp) {
+    const formatTimeUnit = unit => String(unit).length === 1 ? `0${unit}` : unit
+    const date = new Date(timestamp)
+
+    const year = date.getUTCFullYear()
+    const month = formatTimeUnit(date.getUTCMonth() + 1)
+    const day = formatTimeUnit(date.getUTCDate())
+
+    return `${year}-${month}-${day}`
   }
 }
